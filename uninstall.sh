56d931e9454239762a7f7e97d1ac311251895c8a
LeonRein <4107480+LeonRein@users.noreply.github.com>
2025-07-24 11:46:18 +0200

    minor

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 if [ "$UID" == 0 ]; then # If root:
-	ICON_FILE=/usr/share/pixmaps/reboot-to-windows.svg
-	EXECUTABLE=/usr/bin/reboot-to-windows
-	WRAPPER_FILE=/usr/lib/reboot-to-windows-pkexec.sh
-	DESKTOP_FILE=/usr/share/applications/reboot-to-windows.desktop
-	POLICY_FILE=/usr/share/polkit-1/actions/wartybix.reboot-to-windows.policy
-	rm $POLICY_FILE
+    ICON_FILE=/usr/share/pixmaps/reboot-to-windows.svg
+    EXECUTABLE=/usr/bin/reboot-to-windows
+    WRAPPER_FILE=/usr/lib/reboot-to-windows-pkexec.sh
+    DESKTOP_FILE=/usr/share/applications/reboot-to-windows.desktop
+    POLICY_FILE=/usr/share/polkit-1/actions/wartybix.reboot-to-windows.policy
+    rm $POLICY_FILE
 else #If user:
-	ICON_FILE=~/.local/share/icons/reboot-to-windows.svg
-	EXECUTABLE=~/.local/bin/reboot-to-windows.sh
-	WRAPPER_FILE=~/.local/lib/reboot-to-windows-pkexec.sh
-	DESKTOP_FILE=~/.local/share/applications/reboot-to-windows.desktop
+    ICON_FILE=~/.local/share/icons/reboot-to-windows.svg
+    EXECUTABLE=~/.local/bin/reboot-to-windows.sh
+    WRAPPER_FILE=~/.local/lib/reboot-to-windows-pkexec.sh
+    DESKTOP_FILE=~/.local/share/applications/reboot-to-windows.desktop
 fi
 
 rm $ICON_FILE
