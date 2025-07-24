1dc5f897965417e2d07210a1ef726032de054957
LeonRein <4107480+LeonRein@users.noreply.github.com>
2025-07-24 11:54:00 +0200

    reactivated reboot

--- scripts/reboot-to-windows-pkexec.sh
+++ scripts/reboot-to-windows-pkexec.sh
@@ -5,8 +5,7 @@ case "$1" in
         efibootmgr -n "$2"
         ;;
     "reboot")
-        echo "Rebooting system..."
-        # systemctl reboot
+        systemctl reboot
         ;;
     *)
         echo "Usage: $0 {set-boot <boot_id>|reboot}"
