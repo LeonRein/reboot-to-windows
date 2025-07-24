b6a1be5d3238e3aac7affbde0089db37be3d17eb
LeonRein <4107480+LeonRein@users.noreply.github.com>
2025-07-24 11:44:12 +0200

    moved kde_check to install.sh

--- install.sh
+++ install.sh
@@ -23,4 +23,18 @@ install -Dm644 icons/reboot-to-windows.svg "$ICON_LOCATION/reboot-to-windows.svg
 install -Dm755 scripts/reboot-to-windows.sh "$EXECUTABLE_LOCATION"
 install -Dm755 scripts/reboot-to-windows-pkexec.sh "$WRAPPER_LOCATION"
 install -Dm644 desktop/$DESKTOP_FILE "$DESKTOP_FILE_LOCATION"
-sh scripts/kde_check.sh
+
+if [ "$DESKTOP_SESSION" = "plasma" ]; then # If user running KDE Plasma:
+# Check for 'qdbus' command. If empty, set to -1.
+QDBUS=`which qdbus 2>/dev/null || echo -1`
+if [ "$QDBUS" = "-1" ]; then # If qdbus command not found:
+	echo "Install the 'qt' or 'qt5-tools' or similar package from your package manager to provide support for KDE's reboot prompt."
+fi
+fi
+
+if [ $UID == 0 ]; then
+	echo "**********************************************************************"
+	echo "To allow wheel users to reboot to Windows without a password, copy the rules file:"
+	echo "  sudo cp /usr/share/doc/reboot-to-windows-git/rules.d/50-wartybix.reboot-to-windows.rules /etc/polkit-1/rules.d/"
+	echo "**********************************************************************"
+fi
